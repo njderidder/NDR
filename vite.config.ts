1fbda6d9c8bab4bc8b3072c5a93c79d5bd68d0c0
njderidder <njderidder@hotmail.com>
2025-12-29 21:55:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Voeg deze regel toe. Zorg dat dit EXACT overeenkomt met je GitHub repo naam
+      base: '/NDR/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
